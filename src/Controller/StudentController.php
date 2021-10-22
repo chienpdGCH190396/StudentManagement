878d4175152ddf33c9e0707c9af75602847dc44b
hungsapa123lc <65637939+hungsapa123lc@users.noreply.github.com>
2021-10-22 19:27:41 +0700

    Add view all student list controller and view student detail controller, add student index and student detail page in templates

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Student;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StudentController extends AbstractController
 {
-    #[Route('/student', name: 'student')]
-    public function index(): Response
-    {
-        return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
-        ]);
+    #[Route('/student', name: 'student_index')]
+    public function viewAllStudentAction(){
+        $students = new Student();
+        $students = $this->getDoctrine()->getRepository(Student::class)->findAll();
+        return $this->render(
+        'student/index.html.twig', [
+            'students' => $students
+        ]
+        );
     }
+
+    
+      #[Route('/student/detail/{id}', name: 'student_detail')]
+     
+
+    public function studentdetailAction($id){
+        $student = $this->getDoctrine()->getRepository(Student::class)->find($id);
+        if($student == null){
+            $student = $this->addFlash('Error','Student does not exist.');
+            return $this->redirectToRoute('student_index');
+            
+        }else{
+            return $this->render('student/detail.html.twig',[
+                'student' => $student
+            ]);
+        }
+    }
+     /**
+     * @Route("/student/delete/{id}", name="student_delete")
+     */
+
+
+
+     /**
+     * @Route("/student/add", name="student_add")
+     */
+
+
+     /**
+     * @Route("/student/edit/{id}", name="student_edit")
+     */
 }
